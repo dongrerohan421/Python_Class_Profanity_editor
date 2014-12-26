e62cfcbd4b6918497e20f3d5b80d2848fa38c57d
Rohan Dongre <dongrerohan421@gmail.com>
2014-12-26 02:18:09 -0600

    can read content of file

--- check_profanity.py
+++ check_profanity.py
@@ -1,3 +1,7 @@
 def read_text(): # define function to read text
+	quotes = open("/media/rohan/3814F06A14F02C8E/Study/Udacity/Python/Python_Class_Profanity_editor/movie_quotes.txt") # opens file from location
+	content_of_file = quotes.read() #allow to read all content from open file
+	print (content_of_file)
+	quotes.close() # close opened file
 
 read_text() # call function
